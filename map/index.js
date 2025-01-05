3dccb3bc7c0aeef36c900aa7131a3bda1cca1d01
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-01-05 22:36:34 +0800

    Support a query param named cityDir, whose value can be either city or county.

--- map/index.js
+++ map/index.js
@@ -1,5 +1,6 @@
 import * as echarts from 'https://cdn.jsdelivr.net/npm/echarts@5.6.0/dist/echarts.esm.min.js'
-const cityDir = 'city'; // Can be eiher 'city' or 'county'
+const urlParams = new URLSearchParams(window.location.search);
+const cityDir = urlParams.get('cityDir') ?? 'city'; // Can be eiher 'city' or 'county'
 const mapName = '广东广西湖南江西福建';
 const geojson = await fetch(`echarts-china-cities-js/geojson/${cityDir === 'city' ? 'shape-only' : 'shape-with-internal-borders'}/${mapName}.geojson`).then(res => res.json());
 echarts.registerMap(mapName, geojson);
