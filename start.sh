d8453a420044d472566e736e58a2e09eb6a79baf
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-01-10 22:35:22 +0800

    Remove the crawling of www.tianqi.com because of its IP ACL blacklist mechanism.

--- start.sh
+++ start.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 echo "$(date +"%F %T.%N") Script started"
 export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
-echo "$(date +"%F %T.%N") cd weather; node index.js city; cd ..;"
 cd weather
+echo "$(date +"%F %T.%N") node index.js city"
 node index.js city # This script creates screenshots in the `city` directory.
-cd ..
-echo "$(date +"%F %T.%N") cd tianqi; node index.js city; cd ..;"
-cd tianqi
-node index.js city # This script creates screenshots in the `city` directory.
-cd ..
-echo "$(date +"%F %T.%N") cd weather; node index.js county; cd ..;"
-cd weather
-node index.js county # This script creates screenshots in the `county` directory.
-cd ..
-echo "$(date +"%F %T.%N") cd tianqi; node index.js county; cd ..;"
-cd tianqi
+echo "$(date +"%F %T.%N") node index.js county"
 node index.js county # This script creates screenshots in the `county` directory.
 cd ..
 echo "$(date +"%F %T.%N") Script completed"
