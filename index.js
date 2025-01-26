259826b553023e1b0a01e8934f0fcaf2d1bf4aff
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-01-26 14:29:55 +0800

    Fetch more images

--- index.js
+++ index.js
@@ -62,6 +62,16 @@ const hh0113 = ['01','13'][hourIndex];
 	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etm_h000_cn_{date}{hh0820}0000_09600-12000_1920.png',
 	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etm_h000_cn_{date}{hh0820}0000_12000-14400_1920.png',
 	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etm_h000_cn_{date}{hh0820}0000_14400-16800_1920.png',
+	// 全国最低气温预报。图中无标注发布时间
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_00000-02400_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_02400-04800_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_04800-07200_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_07200-09600_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_09600-12000_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_12000-14400_1920.png',
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_grid_etn_h000_cn_{date}{hh0820}0000_14400-16800_1920.png',
+	// 冷空气降温预报图。图中无标注发布时间
+	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_mi_cold_h000_cn_{date}{hh0820}0000_00000-02400_1920.png',
 	// 全国沙尘天气预报图
 	'https://pi.weather.com.cn/i/product/pic/l/cwcc_nmc_fst_web_mi_dust_h000_cn_{date}{hh0820}0000_00000-02400_1920.png',
 	'https://pi.weather.com.cn/i/product/pic/l/sevp_nmc_wtfc_sfer_edust_achn_l88_p9_{date}{hh0012}0002400.jpg',
@@ -89,6 +99,20 @@ await fetchImages('nmc', [
 	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETM_ACHN_L88_P9_{date}{hh0820}0012012.jpg',
 	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETM_ACHN_L88_P9_{date}{hh0820}0014412.jpg',
 	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETM_ACHN_L88_P9_{date}{hh0820}0016812.jpg',
+	// 全国最低气温预报。07时发布08，21时发布20
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0002412.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0004812.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0007212.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0009612.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0012012.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0014412.jpg',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/RFFC/SEVP_NMC_RFFC_SNWFD_ETN_ACHN_L88_P9_{date}{hh0820}0016812.jpg',
+	// 冷空气降温预报图。06时发布00
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/WTFC/SEVP_NMC_WTFC_SFER_ETAMIX1_ACHN_L88_P9_{date}{hh0012}0002400.JPG',
+	// 全国大风预报图。06发布00
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/WTFC/SEVP_NMC_WTFC_SFER_WIND_ACHN_L88_P9_{date}{hh0012}0002400.JPG',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/WTFC/SEVP_NMC_WTFC_SFER_WIND_ACHN_L88_P9_{date}{hh0012}0004800.JPG',
+	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/WTFC/SEVP_NMC_WTFC_SFER_WIND_ACHN_L88_P9_{date}{hh0012}0007200.JPG',
 	// 全国雾预报图。06时发布00，18时发布12
 	'http://image.nmc.cn/product/{YYYY}/{MM}/{DD}/WTFC/SEVP_NMC_WTFC_SFER_EFG_ACHN_L88_P9_{date}{hh0012}0002412.jpg',
 	// 全国霾预报图。06时发布00，17时发布12
