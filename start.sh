01db26f8a6f98e6a8d2c6f8dcd8b0b9373b6ccb1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-01-27 00:06:54 +0800

    Crawl nmc and tianqi in start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,24 @@ echo "$(date +"%F %T.%N") Script started"
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
 export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
+echo "$(date +"%F %T.%N") cd nmc"
+cd nmc
+echo "$(date +"%F %T.%N") node index.js"
+node index.js
+echo "$(date +"%F %T.%N") cd .."
+cd ..
+echo "$(date +"%F %T.%N") cd weather"
 cd weather
 echo "$(date +"%F %T.%N") node index.js city"
 node index.js city # This script creates screenshots in the `city` directory.
 echo "$(date +"%F %T.%N") node index.js county"
 node index.js county # This script creates screenshots in the `county` directory.
+echo "$(date +"%F %T.%N") cd .."
+cd ..
+echo "$(date +"%F %T.%N") cd tianqi"
+cd tianqi
+echo "$(date +"%F %T.%N") node index.js"
+node index.js
+echo "$(date +"%F %T.%N") cd .."
 cd ..
 echo "$(date +"%F %T.%N") Script completed"
