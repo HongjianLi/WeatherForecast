2c553db34fea8f2571866214b29fa4969aa5a79a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-03-21 23:52:25 +0800

    Add 河南12市 陕西5市 甘肃2市

--- map/echarts-china-cities-js/geojson/shape-only/createGeoJson.js
+++ map/echarts-china-cities-js/geojson/shape-only/createGeoJson.js
@@ -3,7 +3,7 @@
 import fs from 'fs/promises';
 const minorities = JSON.parse(await fs.readFile('../../../minorities.json'));
 const features = [].concat(...await Promise.all((await fs.readdir('.')).reduce((cityArr, city) => { // The current directory was cloned from https://github.com/echarts-maps/echarts-china-cityArr-js/tree/master/geojson
-	const provinceIndex = ['xianggang', 'aomen', 'guang3_dong1', 'guang3_xi1', 'hu2_nan2', 'jiang1_xi1', 'fu2_jian4', 'hai3_nan2', 'gui4_zhou1', 'yun2_nan2', 'chongqing', 'si4_chuan1', 'hu2_bei3', 'an1_hui1', 'zhe4_jiang1', 'shanghai', 'jiang1_su1'].findIndex(province => city.startsWith(province));
+	const provinceIndex = ['xianggang', 'aomen', 'guang3_dong1', 'guang3_xi1', 'hu2_nan2', 'jiang1_xi1', 'fu2_jian4', 'hai3_nan2', 'gui4_zhou1', 'yun2_nan2', 'chongqing', 'si4_chuan1', 'hu2_bei3', 'an1_hui1', 'zhe4_jiang1', 'shanghai', 'jiang1_su1', 'he2_nan2_xin4_yang2', 'he2_nan2_zhu4_ma3_dian4', 'he2_nan2_nan2_yang2', 'he2_nan2_zhou1_kou3', 'he2_nan2_ta4_he2', 'he2_nan2_xu3_chang1', 'he2_nan2_ping2_ding3_shan1', 'he2_nan2_luo4_yang2', 'he2_nan2_san1_men2_xia2', 'he2_nan2_shang1_qiu1', 'he2_nan2_kai1_feng1', 'he2_nan2_zheng4_zhou1', 'shan3_xi1_an1_kang1', 'shan3_xi1_han4_zhong1', 'shan3_xi1_shang1_luo4', 'shan3_xi1_xi1_an1', 'shan3_xi1_bao3_ji1', 'gan1_su4_long3_nan2', 'gan1_su4_tian1_shui3'].findIndex(province => city.startsWith(province));
 	if (provinceIndex > -1 && !['guang3_dong1_dong1_sha1_qun2_dao3.geojson', 'hai3_nan2_san1_sha1.geojson'].includes(city)) cityArr.push({ city, provinceIndex }); // Filter out 东沙群岛
 	return cityArr;
 }, []).sort((city0, city1) => (city0.provinceIndex - city1.provinceIndex)).map(async city => {
@@ -21,6 +21,6 @@ const features = [].concat(...await Promise.all((await fs.readdir('.')).reduce((
 	});
 	return features;
 })));
-console.assert(features.length === 194, features.length); // 合共194市 = 香港1市 + 澳门1市 + 广东21市 + 广西14市 + 湖南14市 + 江西11市 + 福建9市 + 海南18市 + 贵州9市 + 云南16市 + 重庆1市 + 四川21市 + 湖北17市 + 安徽16市 + 浙江11市 + 上海1市 + 江苏13市
+console.assert(features.length === 213, features.length); // 合共213市 = 香港1市 + 澳门1市 + 广东21市 + 广西14市 + 湖南14市 + 江西11市 + 福建9市 + 海南18市 + 贵州9市 + 云南16市 + 重庆1市 + 四川21市 + 湖北17市 + 安徽16市 + 浙江11市 + 上海1市 + 江苏13市 + 河南12市(部分) + 陕西5市(部分) + 甘肃2市(部分)
 await fs.writeFile('city.json', JSON.stringify(features.map(feature => ({ city: feature.properties.name })), null, '	'));
 await fs.writeFile(`map.geojson`, JSON.stringify({ type: "FeatureCollection", features }));
--- map/echarts-china-cities-js/geojson/shape-with-internal-borders/createGeoJson.js
+++ map/echarts-china-cities-js/geojson/shape-with-internal-borders/createGeoJson.js
@@ -2,11 +2,11 @@
 // This script creates city.json and map.geojson. It has to be run once only, after ../shape-only/createGeoJson.js.
 import fs from 'fs/promises';
 const cityArr0 = JSON.parse(await fs.readFile('../shape-only/city.json'));
-console.assert(cityArr0.length === 194); // 合共194市 = 香港1市 + 澳门1市 + 广东21市 + 广西14市 + 湖南14市 + 江西11市 + 福建9市 + 海南18市 + 贵州9市 + 云南16市 + 重庆1市 + 四川21市 + 湖北17市 + 安徽16市 + 浙江11市 + 上海1市 + 江苏13市
+console.assert(cityArr0.length === 213); // 合共213市 = 香港1市 + 澳门1市 + 广东21市 + 广西14市 + 湖南14市 + 江西11市 + 福建9市 + 海南18市 + 贵州9市 + 云南16市 + 重庆1市 + 四川21市 + 湖北17市 + 安徽16市 + 浙江11市 + 上海1市 + 江苏13市 + 河南12市(部分) + 陕西5市(部分) + 甘肃2市(部分)
 const featuresArr = await Promise.all([].concat(
 	['xianggang', 'aomen'].map(city => `../shape-only/${city}.geojson`), // Since Hong Kong and Macao are small cities, treat them as a whole and substitute their shape-only version.
 	(await fs.readdir('.')).reduce((cityArr, city) => { // The current directory was cloned from https://github.com/echarts-maps/echarts-china-cityArr-js/tree/master/geojson
-		const provinceIndex = ['guang3_dong1', 'guang3_xi1', 'hu2_nan2', 'jiang1_xi1', 'fu2_jian4', 'hai3_nan2', 'gui4_zhou1', 'yun2_nan2', 'chongqing', 'si4_chuan1', 'hu2_bei3', 'an1_hui1', 'zhe4_jiang1', 'shanghai', 'jiang1_su1'].findIndex(province => city.startsWith(province));
+		const provinceIndex = ['guang3_dong1', 'guang3_xi1', 'hu2_nan2', 'jiang1_xi1', 'fu2_jian4', 'hai3_nan2', 'gui4_zhou1', 'yun2_nan2', 'chongqing', 'si4_chuan1', 'hu2_bei3', 'an1_hui1', 'zhe4_jiang1', 'shanghai', 'jiang1_su1', 'he2_nan2_xin4_yang2', 'he2_nan2_zhu4_ma3_dian4', 'he2_nan2_nan2_yang2', 'he2_nan2_zhou1_kou3', 'he2_nan2_ta4_he2', 'he2_nan2_xu3_chang1', 'he2_nan2_ping2_ding3_shan1', 'he2_nan2_luo4_yang2', 'he2_nan2_san1_men2_xia2', 'he2_nan2_shang1_qiu1', 'he2_nan2_kai1_feng1', 'he2_nan2_zheng4_zhou1', 'shan3_xi1_an1_kang1', 'shan3_xi1_han4_zhong1', 'shan3_xi1_shang1_luo4', 'shan3_xi1_xi1_an1', 'shan3_xi1_bao3_ji1', 'gan1_su4_long3_nan2', 'gan1_su4_tian1_shui3'].findIndex(province => city.startsWith(province));
 		if (provinceIndex > -1 && !['guang3_dong1_dong1_sha1_qun2_dao3.geojson', 'hai3_nan2_san1_sha1.geojson'].includes(city)) cityArr.push({ city, provinceIndex }); // Filter out 东沙群岛
 		return cityArr;
 	}, []).sort((city0, city1) => (city0.provinceIndex - city1.provinceIndex)).map(city => city.city),
@@ -39,7 +39,7 @@ const cityArr = featuresArr.reduce((cityArr, features, index) => {
 	});
 	return cityArr;
 }, []);
-console.assert(cityArr.length === 1414); // 合共1414县 = 香港1市 + 澳门1市 + 广东123县 + 广西111县 + 湖南122县 + 江西100县 + 福建85县 + 海南24县 + 贵州88县 + 云南129县 + 重庆38县 + 四川183县 + 湖北103县 + 安徽105县 + 浙江89县 + 上海16县 + 江苏96县
+console.assert(cityArr.length === 1598); // 合共1598县 = 香港1市 + 澳门1市 + 广东123县 + 广西111县 + 湖南122县 + 江西100县 + 福建85县 + 海南24县 + 贵州88县 + 云南129县 + 重庆38县 + 四川183县 + 湖北103县 + 安徽105县 + 浙江89县 + 上海16县 + 江苏96县 + 河南115县(部分) + 陕西53县(部分) + 甘肃16县(部分)
 await fs.writeFile(`map.geojson`, JSON.stringify({ type: "FeatureCollection", features: cityArr.map(city => city.feature) }));
 cityArr.forEach(city => delete city.feature);
-await fs.writeFile('city.json', JSON.stringify(cityArr, null, '	')); // Both 张家界 and 龙岩 have 永定区. Both 广州 and 贵阳 have 白云区. Both 福州 and 黔东南 have 台江. Both 乐山 and 内江 have 市中区. Both 重庆 and 宁波 have 江北区. Both 南京 and 徐州 have 鼓楼区.
+await fs.writeFile('city.json', JSON.stringify(cityArr, null, '	')); // Both 张家界 and 龙岩 have 永定区. Both 广州 and 贵阳 have 白云区. Both 福州 and 黔东南 have 台江. Both 乐山 and 内江 have 市中区. Both 重庆 and 宁波 have 江北区. 福州、南京、徐州、开封 have 鼓楼区.
