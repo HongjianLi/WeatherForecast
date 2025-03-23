d8b30838154c9e655a0f5b3a77a1484acf6fe02d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-03-23 16:21:18 +0800

    Add 香港 and 澳门 in createGeoJson.js

--- map/poi86/provinces/createGeoJson.js
+++ map/poi86/provinces/createGeoJson.js
@@ -3,7 +3,7 @@
 import fs from 'fs/promises';
 const minorities = JSON.parse(await fs.readFile('../../minorities.json'));
 const features = [].concat(...await Promise.all((await fs.readdir('.')).reduce((cityArr, city) => { // The geojson files were downloaded from https://www.poi86.com/
-	const provinceIndex = ['广东', '广西', '湖南', '江西', '福建', '海南', '贵州', '云南', '重庆', '四川', '湖北', '安徽', '浙江', '上海', '江苏'].findIndex(province => city.startsWith(province));
+	const provinceIndex = ['香港', '澳门', '广东', '广西', '湖南', '江西', '福建', '海南', '贵州', '云南', '重庆', '四川', '湖北', '安徽', '浙江', '上海', '江苏'].findIndex(province => city.startsWith(province));
 	if (provinceIndex > -1) cityArr.push({ city, provinceIndex });
 	return cityArr;
 }, []).sort((city0, city1) => (city0.provinceIndex - city1.provinceIndex)).map(async city => {
