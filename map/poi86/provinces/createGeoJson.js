f72a525c9075c2176804ef7dbe2b3cccd910f758
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-03-23 17:00:44 +0800

    Add poi86 geojson files for more provinces

--- map/poi86/provinces/createGeoJson.js
+++ map/poi86/provinces/createGeoJson.js
@@ -3,7 +3,7 @@
 import fs from 'fs/promises';
 const minorities = JSON.parse(await fs.readFile('../../minorities.json'));
 const features = [].concat(...await Promise.all((await fs.readdir('.')).reduce((cityArr, city) => { // The geojson files were downloaded from https://www.poi86.com/
-	const provinceIndex = ['香港', '澳门', '广东', '广西', '湖南', '江西', '福建', '海南', '贵州', '云南', '重庆', '四川', '湖北', '安徽', '浙江', '上海', '江苏'].findIndex(province => city.startsWith(province));
+	const provinceIndex = ['香港', '澳门', '广东', '广西', '湖南', '江西', '福建', '海南', '贵州', '云南', '重庆', '四川', '湖北', '安徽', '浙江', '上海', '江苏', '河南', '陕西', '甘肃', '山东', '河北', '山西', '宁夏', '青海', '天津', '北京', '内蒙古', '辽宁', '吉林', '黑龙江', '西藏', '新疆'].findIndex(province => city.startsWith(province));
 	if (provinceIndex > -1) cityArr.push({ city, provinceIndex });
 	return cityArr;
 }, []).sort((city0, city1) => (city0.provinceIndex - city1.provinceIndex)).map(async city => {
@@ -11,7 +11,7 @@ const features = [].concat(...await Promise.all((await fs.readdir('.')).reduce((
 	features.forEach(feature => {
 		const { properties } = feature;
 		let { name } = properties;
-		['自治州', '自治县', '林区', '市', '县'].forEach(c => {
+		['自治州', '自治县', '林区', '地区', '盟', '市', '县'].forEach(c => {
 			if (name.endsWith(c)) name = name.slice(0, -c.length);
 		});
 		minorities.forEach(minority => {
