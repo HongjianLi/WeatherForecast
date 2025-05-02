5ceeee5c13fd98f0d459173efbbae0be790d5b8b
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-05-02 18:00:57 +0800

    Backup forecast.json

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "$(date +"%F %T.%N") Script started"
+datetime=$(date +"%F-%H-%M")
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
 export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
@@ -7,14 +8,20 @@ echo "$(date +"%F %T.%N") cd nmc"
 cd nmc
 echo "$(date +"%F %T.%N") node index.js"
 node index.js
+echo "$(date +"%F %T.%N") cp city/forecast.json city/forecast-$datetime.json"
+cp city/forecast.json city/forecast-$datetime.json
 echo "$(date +"%F %T.%N") cd .."
 cd ..
 echo "$(date +"%F %T.%N") cd weather"
 cd weather
 echo "$(date +"%F %T.%N") node index.js city"
 node index.js city # This script creates screenshots in the `city` directory.
+echo "$(date +"%F %T.%N") cp city/forecast.json city/forecast-$datetime.json"
+cp city/forecast.json city/forecast-$datetime.json
 echo "$(date +"%F %T.%N") node index.js county"
 node index.js county # This script creates screenshots in the `county` directory.
+echo "$(date +"%F %T.%N") cp county/forecast.json county/forecast-$datetime.json"
+cp county/forecast.json county/forecast-$datetime.json
 echo "$(date +"%F %T.%N") cd .."
 cd ..
 echo "$(date +"%F %T.%N") cd flight"
