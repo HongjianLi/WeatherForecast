5870579afd11ee6013f2e924c2cbbadd78df02c5
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-05-02 23:33:01 +0800

    Rewrite flight/index.js. Save flight into forecast.json.

--- flight/index.js
+++ flight/index.js
@@ -2,18 +2,7 @@
 import fs from 'fs/promises';
 import puppeteer from 'puppeteer-core';
 import ProgressBar from 'progress';
-const monday = new Date();
-for (var nDaysAhead = 0; monday.getDay() !== 1; ++nDaysAhead) monday.setDate(monday.getDate() + 1); // Find the nearest Monday.
-const depDate = monday.toLocaleDateString('en-CA'/*, { year: 'numeric', month: '2-digit', day: '2-digit'}*/);
-console.log(`Departing on ${depDate}, i.e. ${nDaysAhead} days ahead`);
-const airports = await Promise.all(['CityCode', 'CodeCity'].map(m => fs.readFile(`airports${m}.json`).then(JSON.parse)));
-const dstArr = [].concat(...JSON.parse(await fs.readFile(`../weather/city/forecast.json`)).slice(23).filter(city => { // The first 23 cities are 香港, 澳门 and 广东21市. 无须飞机航班，乘坐高铁即可。
-	const uncomfortableDaysArr = city.forecast.map(f => f.uncomfortable).slice(nDaysAhead); // Skip dates before departure.
-	if (uncomfortableDaysArr[0]) return false; // If the departure date is uncomfortable, skip it.
-	return uncomfortableDaysArr.slice(1).reduce((acc, cur) => { // Sum the number of uncomfortable days after departure, and restrict the sum equal to or below 1.
-		return acc + cur;
-	}, 0) <= 1;
-}).map(city => airports[0][city.city]).filter(airport => airport).map(codeArr => codeArr.slice(0, 1))).reduce((acc, cur) => { if (!acc.includes(cur)) acc.push(cur); return acc; }, []); // codeArr.slice(0, 1) retains the first code for the same city, e.g. CTU for 成都, because ly will return the flights for all airports of the same city, e.g. including TFU.
+const [ forecastArr, city2code/*, code2city*/ ] = await Promise.all(['../weather/city/forecast.json', 'airportsCityCode.json'/*, 'airportsCodeCity.json'*/].map(p => fs.readFile(p).then(JSON.parse))); // Prefer weather to nmc because weather provides the sky key 晴天预报, which indicates whether 灰霾 occurs.
 const browser = await puppeteer.launch({
 	defaultViewport: { width: 1280, height: 2160 }, // Increase the deviceScaleFactor will increase the resolution of screenshots.
 	executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
@@ -28,43 +17,64 @@ await page.setExtraHTTPHeaders({
 	'sec-ch-ua-platform': '"Linux"',
 });
 const srcArr = ['CAN', 'SZX'];
-const bar = new ProgressBar('[:bar] :dst :current/:total=:percent :elapseds :etas', { total: dstArr.length });
-for (const dst of dstArr) {
-	bar.tick({ dst });
-	for (const src of srcArr) {
-		let response;
-		try {
-			response = await page.goto(`https://www.ly.com/flights/itinerary/oneway/${src}-${dst}?date=${depDate}`, { waitUntil: 'networkidle0'} );
-		} catch (error) { // In case of error, e.g. TimeoutError, continue to goto the next dst.
-			console.error(`${depDate}: ${src}-${dst}: page.goto() error ${error}`);
-			continue;
-		}
-		if (response.ok()) {
-			const noFlights = await page.$('div.flight-no-data');
-			if (noFlights !== null) { await noFlights.dispose(); continue }; // If no flights from src to dst, skip it.
-			for (let prevHeight = 0; true;) {
-				await page.evaluate(() => {
-					window.scrollTo(0, document.body.scrollHeight);
-				});
-				await new Promise(resolve => setTimeout(resolve, 2120)); // Wait for some seconds for new contents to load.
-//				await page.waitForNetworkIdle({ idleTime: 2000 }); // Time (in milliseconds) the network should be idle.
-				const newHeight = await page.evaluate(() => document.body.scrollHeight);
-				if (newHeight === prevHeight) break; // Break the loop if no new content is loaded.
-				prevHeight = newHeight;
+const bar = new ProgressBar('[:bar] :city :current/:total=:percent :elapseds :etas', { total: forecastArr.length });
+for (const fc of forecastArr) {
+	const { city, forecast } = fc;
+	bar.tick({ city });
+	if (bar.curr <= 23) continue; // curr starts from one. The first 23 cities are 香港, 澳门 and 广东21市. 无须飞机航班，乘坐高铁即可。
+	const dstArr = city2code[city];
+	if (!dstArr) continue; // Skip cities without airports.
+	const dst = dstArr[0]; // It's sufficient to get just the first code for the city, e.g. CTU for 成都, because ly will return the flights for all airports of the same city, e.g. including TFU.
+	const departDate = new Date();
+	for (let i = 1; i < 5; ++i) {
+		departDate.setDate(departDate.getDate() + 1);
+		const f = forecast[i];
+		if (f.uncomfortable) continue;
+		const n = Math.min(6 - i, 4); // The number of days ahead to check.
+		if ([...Array(n).keys()].reduce((acc, cur) => { // In the following n days, the number of uncomfortable days must be less than half.
+			return acc + forecast[i + 1 + cur].uncomfortable;
+		}, 0) >= n / 2) continue;
+		const departDateString = departDate.toLocaleDateString('en-CA', { year: 'numeric', month: '2-digit', day: '2-digit'});
+		for (const src of srcArr) {
+			let response;
+			try {
+				response = await page.goto(`https://www.ly.com/flights/itinerary/oneway/${src}-${dst}?date=${departDateString}`, { waitUntil: 'networkidle0'} );
+			} catch (error) { // In case of error, e.g. TimeoutError, continue to goto the next dst.
+				console.error(`${departDateString}: ${src}-${dst}: page.goto() error ${error}`);
+				continue;
 			}
-			const flightList = await page.$$('div.flight-lists-container>div.flight-item');
-			for (const flight of flightList) {
-				const price = (await flight.$eval('div.head-prices>strong>em', el => el.innerText)).slice(1); // .slice(1) to filter out the currency symbol ￥.
-				const departTime = await flight.$eval('div.f-startTime>strong', el => el.innerText); // e.g. 08:35
-				const departHour = departTime.slice(0, 2); // e.g. 08
-				if (price < 500 && 10 <= departHour && departHour <= 16) {
-					console.log(`${depDate} ${departTime} ${src}-${dst} ${airports[1][src]}-${airports[1][dst]} ￥${price}`);
+			if (response.ok()) {
+				const noFlights = await page.$('div.flight-no-data');
+				if (noFlights !== null) { await noFlights.dispose(); continue }; // If no flights from src to dst, skip it.
+				for (let prevHeight = 0; true;) {
+					await page.evaluate(() => {
+						window.scrollTo(0, document.body.scrollHeight);
+					});
+					await new Promise(resolve => setTimeout(resolve, 2120)); // Wait for some seconds for new contents to load.
+//					await page.waitForNetworkIdle({ idleTime: 2000 }); // Time (in milliseconds) the network should be idle.
+					const newHeight = await page.evaluate(() => document.body.scrollHeight);
+					if (newHeight === prevHeight) break; // Break the loop if no new content is loaded.
+					prevHeight = newHeight;
 				}
-				await flight.dispose();
+				const flightList = await page.$$('div.flight-lists-container>div.flight-item');
+				for (const flight of flightList) {
+					const price = parseInt((await flight.$eval('div.head-prices>strong>em', el => el.innerText)).slice(1)); // .slice(1) to filter out the currency symbol ￥.
+					const departTime = await flight.$eval('div.f-startTime>strong', el => el.innerText); // e.g. 08:35
+					await flight.dispose();
+					const departHour = departTime.slice(0, 2); // e.g. 08
+					if (10 <= departHour && departHour <= 16) {
+//						console.log(`${code2city[src]}-${code2city[dst]} ${src}-${dst} ${departDateString} ${departTime} ￥${price}`);
+						if (!f.flight || f.flight.price > price) {
+							f.flight = { src, departTime, price };
+						}
+						break; // The flightList is sorted by price ascendingly, so skip processing if the current flight is satisfactory.
+					}
+				}
+			} else {
+				console.error(`${departDateString}: ${src}-${dst}: HTTP response status code ${response.status()}`);
 			}
-		} else {
-			console.error(`${depDate}: ${src}-${dst}: HTTP response status code ${response.status()}`);
 		}
-	}		
+	}
 }
 await browser.close();
+await fs.writeFile(`forecast.json`, JSON.stringify(forecastArr, null, '	'));
--- map/index.js
+++ map/index.js
@@ -5,14 +5,14 @@ const cityDir = urlParams.get('cityDir') ?? 'city'; // Can be eiher 'city' or 'c
 const mapName = '香港澳门广东广西湖南江西福建海南贵州云南重庆四川湖北安徽浙江上海江苏河南陕西甘肃';
 const geojson = await fetch(`echarts-china-cities-js/geojson/${cityDir === 'city' ? 'shape-only' : 'shape-with-internal-borders'}/map.geojson`).then(res => res.json());
 echarts.registerMap(mapName, geojson);
-const forecastArr = await fetch(`../weather/${cityDir}/forecast.json`).then(res => res.json()); // Prefer weather to nmc because weather provides the sky key 晴天预报, which indicates whether 灰霾 occurs.
+const forecastArr = await fetch(`../flight/forecast.json`).then(res => res.json());
 //forecastArr.forEach(fc => fc.forecast.forEach(f => f.uncomfortable = util.isUncomfortable(f))); // Re-evaluate the conditions.
 echarts.init(document.getElementById('mainChart'), 'dark').setOption({
 	tooltip: {
 		formatter: (params) => {
 			const { name, value } = params;
 			const { forecast } = forecastArr.find(city => city.city === name);
-			return `${name} 不舒适天数 ${value}${cityDir === 'city' ? `<br><img src="../nmc/${cityDir}/${name}.webp">` : ''}<br><table width="100%" height="16px"><tr>${forecast.map(f => `<td style="width: 14%; background-color: ${['green', 'orangered'][f.uncomfortable]}"></td>`).join('')}</tr></table><img src="../weather/${cityDir}/${name}.webp">`;
+			return `${name} 不舒适天数 ${value}${cityDir === 'city' ? `<br><img src="../nmc/${cityDir}/${name}.webp">` : ''}<br><table width="100%" height="16px" style="color: white"><tr>${forecast.map(f => `<td style="width: 14%; background-color: ${['green', 'orangered'][f.uncomfortable]}">${f.flight ? `${f.flight.src[0]} ${f.flight.departTime} ${f.flight.price}` : '&nbsp;'}</td>`).join('')}</tr></table><img src="../weather/${cityDir}/${name}.webp">`;
 		},
 	},
 	visualMap: {
