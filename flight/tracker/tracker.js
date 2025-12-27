afe81111455b41a5f7edf725da1b84f6d15e2fb7
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-12-27 23:19:16 +0800

    Support crawling multiple dates, specified in tracker.json

--- flight/tracker/tracker.js
+++ flight/tracker/tracker.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 import fs from 'fs/promises';
 import puppeteer from 'puppeteer-core';
-const now = (new Date()).toLocaleString('zh-CN').replace(/\//g, '-');
-const date = '2026-01-12';
-const srcCity = '广州';
-const dstCity = '西双版纳';
-const city2code = await fs.readFile('../airportsCityCode.json').then(JSON.parse);
-const srcCode = city2code[srcCity][0];
-const dstCode = city2code[dstCity][0];
+const nowLocaleString = (new Date()).toLocaleString('zh-CN');
+const dateStr = nowLocaleString.substring(0, 10).replace(/\//g, '-');
+const timeStr = nowLocaleString.substring(11);
+const [ dsdArr, city2code ] = await Promise.all(['tracker.json', '../airportsCityCode.json'].map(p => fs.readFile(p).then(JSON.parse)));
 const browser = await puppeteer.launch({
 	defaultViewport: { width: 1280, height: 2160 },
 	executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
@@ -20,37 +17,43 @@ await page.setExtraHTTPHeaders({
 	'sec-ch-ua-mobile': '?0',
 	'sec-ch-ua-platform': '"Linux"',
 });
-const response = await page.goto(`https://www.ly.com/flights/itinerary/oneway/${srcCode}-${dstCode}?date=${date}`, { waitUntil: 'networkidle0' });
-console.assert(response.ok());
-const noFlights = await page.$('div.flight-no-data');
-console.assert(noFlights === null);
-for (let prevHeight = 0; true;) {
-	await page.evaluate(() => {
-		window.scrollTo(0, document.body.scrollHeight);
-	});
-	await new Promise(resolve => setTimeout(resolve, 2120)); // Wait for some seconds for new contents to load.
-//	await page.waitForNetworkIdle({ idleTime: 2000 }); // Time (in milliseconds) the network should be idle.
-	const newHeight = await page.evaluate(() => document.body.scrollHeight);
-	if (newHeight === prevHeight) break; // Break the loop if no new content was loaded.
-	prevHeight = newHeight;
+for (const dsd of dsdArr) {
+	const { date, srcCity, dstCity } = dsd;
+	if (date < dateStr) continue; // Skip crawling if the intended travel date has been passed.
+	const srcCode = city2code[srcCity][0];
+	const dstCode = city2code[dstCity][0];
+	const response = await page.goto(`https://www.ly.com/flights/itinerary/oneway/${srcCode}-${dstCode}?date=${date}`, { waitUntil: 'networkidle0' });
+	console.assert(response.ok());
+	const noFlights = await page.$('div.flight-no-data');
+	console.assert(noFlights === null);
+	for (let prevHeight = 0; true;) {
+		await page.evaluate(() => {
+			window.scrollTo(0, document.body.scrollHeight);
+		});
+		await new Promise(resolve => setTimeout(resolve, 2120)); // Wait for some seconds for new contents to load.
+//		await page.waitForNetworkIdle({ idleTime: 2000 }); // Time (in milliseconds) the network should be idle.
+		const newHeight = await page.evaluate(() => document.body.scrollHeight);
+		if (newHeight === prevHeight) break; // Break the loop if no new content was loaded.
+		prevHeight = newHeight;
+	}
+	const flightList = await page.$$('div.flight-lists-container>div.flight-item');
+	const flightArr = await Promise.all(flightList.map(async flight => {
+		const transition = await flight.$('div.f-line-to>div.v-popover'); // e.g. 经停, 华夏联程
+		if (transition !== null) { await transition.dispose(); await flight.dispose(); return; }; // If the current flight has a transition, skip it.
+		const price = parseInt((await flight.$eval('div.head-prices>strong>em', el => el.innerText)).slice(1)); // .slice(1) to filter out the currency symbol ￥.
+		const discount = (await flight.$eval('div.head-prices>i', el => el.innerText)); // e.g. 特惠经济舱, 3.7折经济舱
+		const srcTime = await flight.$eval('div.f-startTime>strong', el => el.innerText); // e.g. 14:25
+		const dstTime = await flight.$eval('div.f-endTime>strong', el => el.innerText); // e.g. 17:25
+		const duration = await flight.$eval('div.f-line-to>i', el => el.innerText); // e.g. 3h0m
+		const durHour = parseInt(duration.split('h')[0]);
+		console.assert(durHour <= 6); // durHour: [1, 6] is acceptable, equivalent to duration: [1h0m, 6h59m].
+		const no = await flight.$eval('p.flight-item-name', el => el.innerText); // e.g. 东方航空MU5742
+		const carrier = await flight.$eval('span.flight-item-type', el => el.innerText); // e.g. 波音737(中)
+		const srcPort = await flight.$eval('div.f-startTime>em', el => el.innerText); // e.g. 白云机场T1
+		const dstPort = await flight.$eval('div.f-endTime>em', el => el.innerText); // e.g. 三义机场
+		await flight.dispose();
+		return `${[ dateStr, timeStr, no, carrier, srcTime, srcPort, duration, dstTime, dstPort, price, discount ].join('	')}\n`;
+	}));
+	await fs.appendFile(`${date}-${srcCity}-${dstCity}.tsv`, flightArr.join(''));
 }
-const flightList = await page.$$('div.flight-lists-container>div.flight-item');
-const flightArr = await Promise.all(flightList.map(async flight => {
-	const transition = await flight.$('div.f-line-to>div.v-popover'); // e.g. 经停, 华夏联程
-	if (transition !== null) { await transition.dispose(); await flight.dispose(); return; }; // If the current flight has a transition, skip it.
-	const price = parseInt((await flight.$eval('div.head-prices>strong>em', el => el.innerText)).slice(1)); // .slice(1) to filter out the currency symbol ￥.
-	const discount = (await flight.$eval('div.head-prices>i', el => el.innerText)); // e.g. 特惠经济舱, 3.7折经济舱
-	const srcTime = await flight.$eval('div.f-startTime>strong', el => el.innerText); // e.g. 14:25
-	const dstTime = await flight.$eval('div.f-endTime>strong', el => el.innerText); // e.g. 17:25
-	const duration = await flight.$eval('div.f-line-to>i', el => el.innerText); // e.g. 3h0m
-	const durHour = parseInt(duration.split('h')[0]);
-	console.assert(durHour <= 6); // durHour: [1, 6] is acceptable, equivalent to duration: [1h0m, 6h59m].
-	const no = await flight.$eval('p.flight-item-name', el => el.innerText); // e.g. 东方航空MU5742
-	const carrier = await flight.$eval('span.flight-item-type', el => el.innerText); // e.g. 波音737(中)
-	const srcPort = await flight.$eval('div.f-startTime>em', el => el.innerText); // e.g. 白云机场T1
-	const dstPort = await flight.$eval('div.f-endTime>em', el => el.innerText); // e.g. 三义机场
-	await flight.dispose();
-	return `${[ now, no, carrier, srcTime, srcPort, duration, dstTime, dstPort, price, discount ].join('	')}\n`;
-}));
 await browser.close();
-await fs.appendFile(`${date}-${srcCity}-${dstCity}.tsv`, flightArr.join(''));
