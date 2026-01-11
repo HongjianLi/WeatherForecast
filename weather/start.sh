ea86decfd963ada436f403ee5c4b320e74000adb
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-11 13:20:17 +0800

    Create the directory 'forecoast' for backup purpose.

--- weather/start.sh
+++ weather/start.sh
@@ -9,11 +9,11 @@ cd weather
 echo "$(date +"%F %T.%N") node index.js city"
 node index.js city
 echo "$(date +"%F %T.%N") cp city/forecast.json city/forecast-$datetime.json"
-cp city/forecast.json city/forecast-$datetime.json
+cp city/forecast.json city/forecast/$datetime.json # Backup
 echo "$(date +"%F %T.%N") node index.js county"
 node index.js county
 echo "$(date +"%F %T.%N") cp county/forecast.json county/forecast-$datetime.json"
-cp county/forecast.json county/forecast-$datetime.json
+cp county/forecast.json county/forecast/$datetime.json # Backup
 echo "$(date +"%F %T.%N") cd .."
 cd ..
 echo "$(date +"%F %T.%N") Script completed"
