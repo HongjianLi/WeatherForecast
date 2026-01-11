6564a38fc959b0eb521d19ca1937b00a4e6c2d39
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-11 11:26:13 +0800

    Add README.md, .gitignore, start.sh for flight

---
+++ flight/README.md
@@ -0,0 +1,5 @@
+# flight
+
+Flight-based applications, including
+* tracking flight prices (directory `tracker`)
+* searching for flights to destination cities with forecasted comfortable weather (file `index.js`)
---
+++ flight/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+echo "$(date +"%F %T.%N") Script started"
+export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
+echo "$(date +"%F %T.%N") node index.js"
+node index.js
+echo "$(date +"%F %T.%N") cd tracker"
+cd tracker
+echo "$(date +"%F %T.%N") node tracker.js"
+node tracker.js
+cd ..
+echo "$(date +"%F %T.%N") Script completed"
