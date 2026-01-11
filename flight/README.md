7795e79d292eb03d984f23050072c345cbbaf62f
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-11 11:38:19 +0800

    git mv tracker/* .

--- flight/README.md
+++ flight/README.md
@@ -1,5 +1,5 @@
 # flight
 
 Flight-based applications, including
-* tracking flight prices (directory `tracker`)
+* tracking flight prices (files `index.js` and `index.html`)
 * searching for flights to destination cities with forecasted comfortable weather (file `weather.js`)
--- flight/start.sh
+++ flight/start.sh
@@ -3,9 +3,6 @@ echo "$(date +"%F %T.%N") Script started"
 export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
 echo "$(date +"%F %T.%N") node weather.js"
 node weather.js
-echo "$(date +"%F %T.%N") cd tracker"
-cd tracker
-echo "$(date +"%F %T.%N") node tracker.js"
-node tracker.js
-cd ..
+echo "$(date +"%F %T.%N") node index.js"
+node index.js
 echo "$(date +"%F %T.%N") Script completed"
