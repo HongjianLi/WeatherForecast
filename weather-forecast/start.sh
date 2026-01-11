785208e5a256fce415009a76beb3f527bd13deda
Jacky Lee <JackyLeeHongJian@Gmail.com>
2026-01-11 12:20:43 +0800

    Separate flight tracker from weather-forecast

--- weather-forecast/start.sh
+++ weather-forecast/start.sh
@@ -7,22 +7,13 @@ export PUPPETEER_EXECUTABLE_PATH=/opt/google/chrome/chrome
 echo "$(date +"%F %T.%N") cd weather"
 cd weather
 echo "$(date +"%F %T.%N") node index.js city"
-node index.js city # This script creates screenshots in the `city` directory.
+node index.js city
 echo "$(date +"%F %T.%N") cp city/forecast.json city/forecast-$datetime.json"
 cp city/forecast.json city/forecast-$datetime.json
 echo "$(date +"%F %T.%N") node index.js county"
-node index.js county # This script creates screenshots in the `county` directory.
+node index.js county
 echo "$(date +"%F %T.%N") cp county/forecast.json county/forecast-$datetime.json"
 cp county/forecast.json county/forecast-$datetime.json
 echo "$(date +"%F %T.%N") cd .."
 cd ..
-echo "$(date +"%F %T.%N") cd flight"
-cd flight
-echo "$(date +"%F %T.%N") node index.js"
-node index.js
-echo "$(date +"%F %T.%N") cd tracker"
-cd tracker
-echo "$(date +"%F %T.%N") node tracker.js"
-node tracker.js
-cd ../..
 echo "$(date +"%F %T.%N") Script completed"
